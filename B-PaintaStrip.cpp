c0bbde4ff69fb256bfc39ec8af3c8ea93746afe5
darsheel-sharma <darsheelsharma528@gmail.com>
2026-01-16 15:01:50 +0530

    'B - Paint a Strip

---
+++ B-PaintaStrip.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+bool isPrime(long long n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+
+    if (n % 2 == 0 || n % 3 == 0) return false;
+
+    for (long long i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+bool isPow2(int n) {
+    return n > 0 && (n & (n - 1)) == 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+       int n;
+       cin >> n;
+       int ans = 1;
+       int initial = 2;
+       int prev = 0;
+       if(n == 1) {
+        cout << 1 << "\n";
+        continue;
+       }
+       while(n > 0) {
+        ans++;
+        initial = initial*2;
+        n -= initial - prev;
+        prev = initial;
+        initial++;
+       }
+       cout << ans << "\n";
+    }
+    return 0;
+}
